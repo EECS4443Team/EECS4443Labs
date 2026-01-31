a56358cfee452c42b045020298d081b038425b8d
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2026-01-31 14:17:41 -0500

    added cancel button behaviour

--- app/src/main/java/com/example/eecs4443lab/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/eecs4443lab/ui/login/LoginActivity.java
@@ -2,25 +2,21 @@ package com.example.eecs4443lab.ui.login;
 
 import android.app.Activity;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.eecs4443lab.HomeActivity;
@@ -53,6 +49,7 @@ public class LoginActivity extends AppCompatActivity {
         final Button loginButton = binding.buttonLogin;
         final ProgressBar loadingProgressBar = binding.loading;
         final Button registerButton = binding.buttonRegister;
+        final Button cancelButton = binding.button;
 
         TextMaskToggleUtil.attach(
                 passwordBinding.editTextPasswordInput,
@@ -60,40 +57,34 @@ public class LoginActivity extends AppCompatActivity {
         );
 
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
+        loginViewModel.getLoginFormState().observe(this, loginFormState -> {
+            if (loginFormState == null) {
+                return;
+            }
+            loginButton.setEnabled(loginFormState.isDataValid());
+            if (loginFormState.getUsernameError() != null) {
+                usernameEditText.setError(getString(loginFormState.getUsernameError()));
+            }
+            if (loginFormState.getPasswordError() != null) {
+                passwordEditText.setError(getString(loginFormState.getPasswordError()));
             }
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-
-                }
-                setResult(Activity.RESULT_OK);
-
+        loginViewModel.getLoginResult().observe(this, loginResult -> {
+            if (loginResult == null) {
+                return;
+            }
+            loadingProgressBar.setVisibility(View.GONE);
+            if (loginResult.getError() != null) {
+                showLoginFailed(loginResult.getError());
+            }
+            if (loginResult.getSuccess() != null) {
+                updateUiWithUser(loginResult.getSuccess());
 
             }
+            setResult(Activity.RESULT_OK);
+
+
         });
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
@@ -125,8 +116,19 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+        cancelButton.setOnClickListener(v -> {
+            usernameEditText.setText("");
+            passwordEditText.setText("");
 
-            loginButton.setOnClickListener(v -> {
+            usernameEditText.setError(null);
+            passwordEditText.setError(null);
+            usernameEditText.requestFocus();
+
+            loadingProgressBar.setVisibility(View.GONE);
+
+        });
+
+        loginButton.setOnClickListener(v -> {
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
