9631f749a1534bf6907e272e9d04ff81dfcaf06f
Jorel Louie Chim <116990156+JorelC18@users.noreply.github.com>
2026-01-31 18:33:22 -0500

    Removed unused import statement

--- app/src/main/java/com/example/eecs4443lab/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/eecs4443lab/ui/login/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
